3c30a9cc0a9f7f901af2eb2a8e8d0ac4c97e409f
gedeck <peter.gedeck@gmail.com>
2022-08-21 18:44:49 -0400

    Fix tests (#4)
    * Fix tests
    
    * Update post proofs

--- ModernStatistics/code/Chap006.py
+++ ModernStatistics/code/Chap006.py
@@ -74,7 +74,7 @@ model_1.predict(dow1941_df).plot(ax=ax, color='black')
 plt.show()
 
 dow1941_df = tsatools.add_trend(dow1941_ts, trend='ct')
-formula = 'Dow_Jones_Index ~ np.power(trend,3) + np.power(trend,2) + trend + 1'
+formula = 'Dow_Jones_Index ~ I(trend**3) + I(trend**2) + trend + 1'
 model_2 = smf.ols(formula=formula, data=dow1941_df).fit()
 print(model_2.params)
 print(f'r2-adj: {model_2.rsquared_adj:.3f}')
@@ -87,7 +87,7 @@ plt.show()
 
 dow1941_df = tsatools.add_trend(dow1941_ts, trend='ct')
 dow1941_df['month'] = dow1941_df.index.month
-poly_formula = 'Dow_Jones_Index ~ C(month) + np.power(trend, 3) + np.power(trend, 2) + trend + 1'
+poly_formula = 'Dow_Jones_Index ~ C(month) + I(trend**3) + I(trend**2) + trend + 1'
 model_3 = smf.ols(formula=poly_formula, data=dow1941_df).fit()
 print(model_3.params)
 print(f'r2-adj: {model_3.rsquared_adj:.3f}')
